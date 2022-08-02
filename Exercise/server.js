a2c6994b96595bf256ed2141c4776f6c52656727
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-08-02 16:35:28 +0600

    M

--- Exercise/server.js
+++ Exercise/server.js
@@ -1,3 +1,11 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  // res.write("Welcome");
+  // res.write("hello world");
+  res.end();
+});
+
+server.listen(3000);
+
+console.log("server listing on port 3000");
