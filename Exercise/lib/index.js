76bba4b28a6ba91968b43f111c6184d3796078a0
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-08-08 14:37:40 +0600

    U

---
+++ Exercise/lib/index.js
@@ -0,0 +1,14 @@
+const mathLibrary = requrire("./math.js");
+const quaternionLibrary = require("./quate");
+
+const app = {};
+
+app.config = {
+  timeBetweenQuteos: 1000,
+};
+
+app.printAQute = function printAQute() {};
+
+app.indefiniteLoop = function indefiniteLoop() {};
+
+app.indefiniteLoop();
---
+++ Exercise/lib/math.js
@@ -0,0 +1,8 @@
+const math = {};
+
+math.printRandomNumber = function printRandomNumber(min, max) {
+  let minimum = min;
+  let maximum = max;
+};
+
+module.export = math;
