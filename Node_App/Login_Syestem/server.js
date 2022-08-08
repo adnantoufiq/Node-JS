07254b8d82bac734f11768c0f7d0fdc243532415
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-08-08 15:14:30 +0600

    M

--- Node_App/Login_Syestem/server.js
+++ Node_App/Login_Syestem/server.js
@@ -9,3 +9,7 @@ app.set("View engine", "ejs");
 app.get("/", (req, res) => {
   res.render("base", { title: "Login System" });
 });
+
+app.listen(port, () => {
+  console.log("Listening to server on port on http://localhost:3000");
+});
