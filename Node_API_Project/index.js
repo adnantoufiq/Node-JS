a1ba565874afe01ad43cd639d6af2d07ca7e4e85
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-08-11 09:59:01 +0600

    M

--- Node_API_Project/index.js
+++ Node_API_Project/index.js
@@ -3,21 +3,20 @@
 
 // dependencies for this projects
 const http = require("http");
-const url = require("url");
+// imports from another folder
+const { handleReqRes } = require("./helpers/handleReqRes");
 
 // app object
 const app = {};
 
 // configuration object
-
 app.config = {
   port: 3000,
 };
 
 // create server
-
 app.createServer = () => {
-  const server = http.createServer(app.handleRequest);
+  const server = http.createServer(app.handleReqRes);
 
   server.listen(app.config.port, () => {
     console.log(`Listening on port " + ${app.config.port}`);
@@ -26,18 +25,6 @@ app.createServer = () => {
 
 // handle requestes and responses
 
-app.handleRequest = (req, res) => {
-  // requests handled by users
-
-  // url and parse it
-
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const method = req.method.toUpperCase();
-  const headers = req.headers.console.log(method);
-
-  // response handle from server
-  res.end("hello new world ");
-};
+app.handleReqRes = handleReqRes;
 
 app.createServer();
