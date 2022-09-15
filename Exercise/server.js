5fd58b80d11822f0a7d4ac6f3d42c79302899a7c
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-09-15 08:54:28 +0600

    U

--- Exercise/server.js
+++ Exercise/server.js
@@ -1,12 +1,21 @@
-const http = require("http");
-const fs = require("fs");
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 
 http
   .createServer(function (req, res) {
+    var p = url.parse(req.url, true);
+    var filename = "." + p.pathname;
+    //Open a file on the server and return its content:
     fs.readFile("index.html", function (err, data) {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(data);
-      res.end();
+      if (err) {
+        res.writeHead(404, { "Content-type": "text/html" });
+        return res.end("there was and error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
     });
   })
   .listen(8080);
