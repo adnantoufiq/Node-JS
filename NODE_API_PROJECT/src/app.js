f528e4fc9613fa3af1758c3c5dd8343137c9e9bb
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-11-02 10:18:33 +0600

    Merge

--- NODE_API_PROJECT/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-// const port = 8080;
-
-app.listen(3090, (req, res) => {
-  console.log("server run on 3090");
-});
--- ULTRA-X Project/src/tes.js
+++
@@ -1,9 +0,0 @@
-
-const { jsPDF } = require("jspdf"); // will automatically load the node version
-
-
-// Default export is a4 paper, portrait, using millimeters for units
-const doc = new jsPDF();
-
-doc.text("Hello world!", 10, 10);
-doc.save("a4.pdf");
