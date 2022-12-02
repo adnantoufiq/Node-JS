4dc37341aabb82ebd098a38a8ac91b99a63d8c17
Ariful Islam Toufiq <78754445+adnantoufiq@users.noreply.github.com>
2022-12-02 16:02:59 +0600

    Delete ULTRA-X Project directory

--- ULTRA-X Project/src/libs/evidence-pdf/get-offset/get-offset.js
+++
@@ -1,8 +0,0 @@
-const getXOffset = (text, document) => {
-    const xOffset = (document.internal.pageSize.width / 2) - (document.getStringUnitWidth(text) * document.internal.getFontSize() / 2);
-
-    return xOffset;
-}
-module.exports={
-    getXOffset
-}
