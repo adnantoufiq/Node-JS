a1e56006665553c85aae1bfdc5260e67e54ce5cb
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-05 09:30:39 +0600

    authentication file

--- ULTRA-X Project/IMS_UXHS_TRAINING-Main/src/middlewares/auth-token/authenticate-token.js
+++ ULTRA-X Project/IMS_UXHS_TRAINING-Main/src/middlewares/auth-token/authenticate-token.js
@@ -1,47 +1,41 @@
-// AUTHORIZED BY MAJEDUL
-
-require('dotenv').config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 /**
- * @author Md. Majedul Islam, 
+ * @author Ariful Islam Toufiq,
  * Software Engineer,
  * Ultra-X BD Ltd.
  *
  * @copyright All right reserved Ultra-X Asia Pacific
- * 
- * @description 
- * 
+ *
+ * @description
+ *
  */
 const jwtAuthenticate = (req, res, next) => {
+  const authHeader = (req.headers["authorization"] || req.body.token) + "";
+  let token = authHeader.replace("Bearer ", "");
+  token = authHeader.replace("Bearer ", "");
 
-	const authHeader = (req.headers['authorization'] || req.body.token) + '';
-	let token = authHeader.replace('Bearer ', '');
-	token = authHeader.replace('Bearer ', '');
+  if (token === null) {
+    return res.status(401).send({
+      status: "failed",
+      message: "unauthorized-user",
+    });
+  }
 
-	if (token === null) {
-		return res.status(401).send({
-			status: 'failed',
-			message: 'unauthorized-user'
-		})
-	}
-
-	try {
-		const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-		if (user) {
-			next();
-		}
-	} catch (error) {
-		return res.status(200)
-			.send({
-				status: 'failed',
-				isValidToken: false,
-				message: "toke-is-not-valid"
-			});
-	}
-
-
-}
+  try {
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (user) {
+      next();
+    }
+  } catch (error) {
+    return res.status(200).send({
+      status: "failed",
+      isValidToken: false,
+      message: "toke-is-not-valid",
+    });
+  }
+};
 module.exports = {
-	jwtAuthenticate
-}
+  jwtAuthenticate,
+};
