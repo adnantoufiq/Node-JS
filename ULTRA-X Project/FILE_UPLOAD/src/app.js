fe3e487d79da91d7d3f613d2bd58d4128e2dd498
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-09 12:25:46 +0600

    app file

---
+++ ULTRA-X Project/FILE_UPLOAD/src/app.js
@@ -0,0 +1,16 @@
+// import module
+
+const { fileUploads } = require("./libs/main/file-upload");
+
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.json());
+
+app.use("/uploads", fileUploads);
+
+app.listen(4560, () => {
+  console.log(`The app is listening at http://localhost:4560`);
+});
