eecb962d734569d9eee6bb483037a6f45f90ee54
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-09 15:55:46 +0600

    Path normalize and file name modifiers

--- ULTRA-X Project/FILE_UPLOAD/src/libs/nameStorage/storage-filename-modifiers.js
+++ ULTRA-X Project/FILE_UPLOAD/src/libs/nameStorage/storage-filename-modifiers.js
@@ -3,12 +3,13 @@ const multer = require("multer");
 
 //folder destination
 const UPLOAD_FOLDER = path.join(__dirname, "../uploads/");
+const normalize_path_folder = path.normalize(UPLOAD_FOLDER);
 
 //disk storage
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, UPLOAD_FOLDER);
+    cb(null, normalize_path_folder);
   },
   filename: (req, file, cb) => {
     const fileExt = path.extname(file.originalname);
