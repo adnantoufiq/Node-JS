464a520af5a0ddbba839b1eed11a9f228613e51b
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-09 16:12:33 +0600

    upload image validation

--- ULTRA-X Project/FILE_UPLOAD/src/libs/validation/image-file-validation.js
+++ ULTRA-X Project/FILE_UPLOAD/src/libs/validation/image-file-validation.js
@@ -6,7 +6,6 @@ const { imageFileSize } = require("../fileSize/all-file-size-define");
 const imageValidation = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const _files = ["png", "jpeg", "jpg", "gif", "svg", "webp"];
     const _file_types = [
       "image/png",
       "image/jpeg",
@@ -16,16 +15,7 @@ const imageValidation = multer({
       "image/webp",
     ];
 
-    // Get the extension of the uploaded file
-    const file_extension = file.originalname.slice(
-      ((file.originalname.lastIndexOf(".") - 1) >>> 0) + 2
-    );
-    // console.log(file_extension);
-
-    if (
-      !_files.includes(file_extension) ||
-      !_file_types.includes(file.mimetype)
-    ) {
+    if (!_file_types.includes(file.mimetype)) {
       cb(new Error("Invalid Data"));
     } else {
       cb(null, true);
