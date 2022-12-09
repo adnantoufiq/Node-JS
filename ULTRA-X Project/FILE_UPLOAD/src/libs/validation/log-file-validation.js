d8cabccfedd6958851b67354414f3fc2fe86b037
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-09 16:12:51 +0600

    log file validation

--- ULTRA-X Project/FILE_UPLOAD/src/libs/validation/log-file-validation.js
+++ ULTRA-X Project/FILE_UPLOAD/src/libs/validation/log-file-validation.js
@@ -5,14 +5,10 @@ const { logFileSize } = require("../fileSize/all-file-size-define");
 const logValidation = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    if (file.fieldname === "log") {
-      if (file.mimetype === "application/octet-stream") {
-        cb(null, true);
-      } else {
-        cb(new Error("ONly .log file allowed!"));
-      }
+    if (file.mimetype === "application/octet-stream") {
+      cb(null, true);
     } else {
-      cb(new Error("There was an unknown error!"));
+      cb(new Error("ONly .log file allowed!"));
     }
   },
   limits: {
