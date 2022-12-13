ac8e2218c8254069c32f145ae3bca1b4dc84ffeb
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-12-13 14:32:48 +0600

    all file size&type define file

---
+++ ULTRA-X Project/FILE_UPLOAD/src/libs/fileSizeType/file-size-type-define.js
@@ -0,0 +1,19 @@
+// image file types
+const _file_types = [
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "image/gif",
+  "image/svg+xml",
+  "image/webp",
+];
+
+const imageFileSize = 50000000; // 50MB
+
+const logFileSize = 70000000; //70MB
+
+module.exports = {
+  imageFileSize,
+  logFileSize,
+  _file_types,
+};
